ee796cf891835271e52328fbae3fe4b0947e5cd6
emmangaspar <emmanuel.gaspar@softvision.com>
2023-01-24 17:17:36 +0800

    US 125 Emman - Final Optimization of Page Models and Test Scripts

--- CommunityTrackerAutomation/Models/WebPage/Selenium/CreateEmployeePageModel.cs
+++ CommunityTrackerAutomation/Models/WebPage/Selenium/CreateEmployeePageModel.cs
@@ -39,14 +39,14 @@ namespace Models.WebPage.Selenium
         /// </summary>
         private LazyElement CSVMailTextbox
         {
-            get { return this.GetLazyElement(By.CssSelector("input[id='email']"), "CSV Mail"); }
+            get { return this.GetLazyElement(By.Id("email"), "CSV Mail"); }
         }
         /// <summary>
         /// Gets Cognizant ID
         /// </summary>
         private LazyElement CognizantIDTextbox
         {
-            get { return this.GetLazyElement(By.CssSelector("input#cognizantId"), "Cognizant ID"); }
+            get { return this.GetLazyElement(By.CssSelector("input[name='cognizantId']"), "Cognizant ID"); }
         }
         /// <summary>
         /// Gets Work State
@@ -55,7 +55,7 @@ namespace Models.WebPage.Selenium
         /// <returns></returns>
         private LazyElement WorkStateCombobox//(string index)
         {
-            get { return this.GetLazyElement(By.CssSelector("div#mui-component-select-state"), "Work State"); }
+            get { return this.GetLazyElement(By.Id("mui-component-select-state"), "Work State"); }
             //return this.GetLazyElement(By.Id($"li[role='option'][data-value={index}]"), "Work State"); 
         }
         /// <summary>
@@ -63,42 +63,42 @@ namespace Models.WebPage.Selenium
         /// </summary>
         private LazyElement JobLevelCombobox
         {
-            get { return this.GetLazyElement(By.CssSelector("div#mui-component-select-jobLevel"), "Job Level"); }
+            get { return this.GetLazyElement(By.Id("mui-component-select-jobLevel"), "Job Level"); }
         }
         /// <summary>
         /// Gets Hired Date
         /// </summary>
         private LazyElement HiredDateCombobox
         {
-            get { return this.GetLazyElement(By.CssSelector("div[class*='MuiOutlinedInput-root'] input[name='hiredDate']"), "Hired Date"); }
+            get { return this.GetLazyElement(By.CssSelector("input[name='hiredDate']"), "Hired Date"); }
         }
         /// <summary>
         /// Gets Project
         /// </summary>
         private LazyElement ProjectCombobox
         {
-            get { return this.GetLazyElement(By.CssSelector("div[class*='MuiGrid-root'] div#mui-component-select-project"), "Project"); }
+            get { return this.GetLazyElement(By.Id("mui-component-select-project"), "Project"); }
         }
         /// <summary>
         /// Gets Skills
         /// </summary>
         private LazyElement SkillsCombobox
         {
-            get { return this.GetLazyElement(By.CssSelector("div[class*='MuiFormControl-root'] input[placeholder='Skills']"), "Skills"); }
+            get { return this.GetLazyElement(By.Id("auto-complete-chip"), "Skills"); }
         }
         /// <summary>
         /// Gets Probationary
         /// </summary>
         private LazyElement ProbationaryButton
         {
-            get { return this.GetLazyElement(By.CssSelector("div[class*='MuiGrid-root'] span[class*='MuiSwitch-track']"), "Probationary"); }
+            get { return this.GetLazyElement(By.XPath("//*[text() = 'Probationary']/preceding-sibling::span"), "Probationary"); }
         }
         /// <summary>
         /// Gets Bench Tags
         /// </summary>
         private LazyElement BenchTagsCheckbox
         {
-            get { return this.GetLazyElement(By.CssSelector("div.MuiContainer-root.MuiContainer-maxWidthXl.css-1spqbit-MuiContainer-root:nth-child(2) div.MuiBox-root.css-k008qs:nth-child(3) main.css-yfo96e div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.MuiCard-root.css-93edo7-MuiPaper-root-MuiCard-root div.MuiBox-root.css-0 form.MuiGrid-root.MuiGrid-container.MuiGrid-direction-xs-column.css-l6hioc-MuiGrid-root div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-12.MuiGrid-grid-md-12.MuiGrid-grid-lg-12.css-1blmxdp-MuiGrid-root div.MuiGrid-root.MuiGrid-container.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-12.MuiGrid-grid-md-12.MuiGrid-grid-lg-12.css-1gd44eh-MuiGrid-root div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-sm-12.MuiGrid-grid-md-12.MuiGrid-grid-lg-11.css-ro7t2c-MuiGrid-root:nth-child(11) div.MuiGrid-root.MuiGrid-container.css-11lq3yg-MuiGrid-root div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-6.css-1fyyp8j-MuiGrid-root:nth-child(1) label.MuiFormControlLabel-root.MuiFormControlLabel-labelPlacementEnd.css-1t4oj1i-MuiFormControlLabel-root > span.MuiCheckbox-root.MuiCheckbox-colorPrimary.MuiButtonBase-root.MuiCheckbox-root.MuiCheckbox-colorPrimary.PrivateSwitchBase-root.css-qv24h5-MuiButtonBase-root-MuiCheckbox-root"), "Bench Tags"); }
+            get { return this.GetLazyElement(By.XPath("//*[text() = 'Bench Tags']/preceding-sibling::span"), "Bench Tags"); }
         }
         /// <summary>
         /// GoP Account
@@ -278,7 +278,7 @@ namespace Models.WebPage.Selenium
         /// Select Random Probationary
         /// </summary>
         /// <param name=""></param>
-        public void selectThreeRandomProbationaryOption()
+        public void selectFiveRandomProbationaryOption()
         {
             Random random = new Random();
             var listOfCheckboxSelectors = new List<LazyElement>()
@@ -301,7 +301,7 @@ namespace Models.WebPage.Selenium
                 FifthMonthTouchpointMeetingCheckbox
                 
             };
-            for(int i = 0; i < 3; i++)
+            for(int i = 0; i < 5; i++)
             {
                 int randomizer = random.Next(0, 15);
                 listOfCheckboxSelectors[randomizer].Click();
@@ -353,12 +353,12 @@ namespace Models.WebPage.Selenium
             // If probatinary is true will select a random series of checkbox for probationary employee
             if (probationary == true)
             {
-                selectThreeRandomProbationaryOption();
+                selectFiveRandomProbationaryOption();
             }
             else
             {
                 employeeRecordPageModel.ClickSkillsInputField();
-                //ProbationaryButton.Click();
+                ProbationaryButton.Click();
             }
             SaveButton.Click();
         }
