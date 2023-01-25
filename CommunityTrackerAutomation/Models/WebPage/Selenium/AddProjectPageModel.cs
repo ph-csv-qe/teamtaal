9b70446edd1cd3c8d3e2ca3801d49053b3f0e5f8
Jasper Liwanag <223657@cognizant.com>
2023-01-25 17:27:23 +0800

    Optimization updates for AddProject

--- CommunityTrackerAutomation/Models/WebPage/Selenium/AddProjectPageModel.cs
+++ CommunityTrackerAutomation/Models/WebPage/Selenium/AddProjectPageModel.cs
@@ -25,41 +25,43 @@ namespace Models.WebPage.Selenium
         /// </summary>
         private LazyElement LandingPageHamburgerButton
         {
-            get { return this.GetLazyElement(By.XPath("//*[@id='root']/div[2]/div[2]/button")); }
+            get { return this.GetLazyElement(By.CssSelector("button[type='button'] svg[data-testid='ArrowForwardIosIcon']"),"Menu Button"); }
+            //get { return this.GetLazyElement(By.XPath("//*[@id='root']/div[2]/div[2]/button")); }
         }
         private LazyElement MaintenanceButton
         {
-            get { return this.GetLazyElement(By.XPath("//*[@id='root']/div[2]/div[3]/div/div/div[2]/div[1]/ul/div[6]/div/div[2]/span")); }
-            //get { return this.GetLazyElement(By.XPath("//svg[data-testid=\'ConstructionIcon\']")); }
+            get { return this.GetLazyElement(By.CssSelector("div[role='button'] svg[data-testid='ConstructionIcon']"), "Maintenance Button"); }
+            //get { return this.GetLazyElement(By.XPath("//*[@id='root']/div[2]/div[3]/div/div/div[2]/div[1]/ul/div[6]/div/div[2]/span")); }
         }
         private LazyElement ProjectTab
         {
-            get { return this.GetLazyElement(By.XPath("//button[@id='1']")); }
-            //
+            get { return this.GetLazyElement(By.CssSelector("button[id='1']"), "Project Tab"); }
+            //get { return this.GetLazyElement(By.XPath("//button[@id='1']")); }
         }
         private LazyElement AddProjectButton
         {
-            get { return this.GetLazyElement(By.XPath("//*[@id='1']/div/span/div[1]/button")); }
-            //get { return this.GetLazyElement(By.XPath("//div[id=\"1\"] button svg[data-testid='AddCircleOutlineIcon']")); }
+            get { return this.GetLazyElement(By.CssSelector("button[type='button'] svg[data-testid='AddCircleOutlineIcon']"), "Add Project Button"); }
+            //get { return this.GetLazyElement(By.XPath("//*[@id='1']/div/span/div[1]/button")); }
         }
         private LazyElement AddProjectName
         {
-            get { return this.GetLazyElement(By.Id("name")); }
+            get { return this.GetLazyElement(By.Id("name"), "Add Project Form - Project Name"); }
         }
         private LazyElement AddProjectCode
         {
-            get { return this.GetLazyElement(By.Id("code")); }
+            get { return this.GetLazyElement(By.Id("code"), "Add Project Form - Project Code"); }
         }
         private LazyElement AddProjectDetailsButton
         {
-            get { return this.GetLazyElement(By.XPath("/html/body/div[3]/div[3]/div/div[2]/button[2]")); }
+            get { return this.GetLazyElement(By.CssSelector("div.MuiDialogActions-root>button:nth-child(2)"), "Add Project Form - Add Button"); }
+            //get { return this.GetLazyElement(By.XPath("/html/body/div[3]/div[3]/div/div[2]/button[2]")); }
         }
 
         public void SwitchToMainWindow()
         {
             WebDriver.SwitchTo().Window(WebDriver.WindowHandles[0]);
         }
-        public void ReachAddProject(string password)
+        public void ReachAddProject()
         {
             SwitchToMainWindow();
             LandingPageHamburgerButton.Click();
---
+++ CommunityTrackerAutomation/Tests/AddProjectTests.cs
@@ -0,0 +1,61 @@
+﻿using CognizantSoftvision.Maqs.BaseDatabaseTest;
+using CognizantSoftvision.Maqs.BaseSeleniumTest;
+using CognizantSoftvision.Maqs.BaseWebServiceTest;
+using CognizantSoftvision.Maqs.Utilities.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models.WebPage.Selenium;
+using System;
+using System.Data;
+using System.Linq;
+using System.Threading;
+
+namespace Tests
+{
+    /// <summary>
+    /// Composite Add New Project Test Class
+    /// Author: Jasper Liwanag
+    /// </summary>
+    [TestClass]
+    public class AddProjectTests : BaseSeleniumTest
+    {
+        /// <summary>
+        /// Do database setup for test run
+        /// </summary>
+        // [ClassInitialize] - Disabled because this step will fail as the template does not include access to a test database
+        public static void TestSetup(TestContext context)
+        {
+            // Do database setup
+            using (DatabaseDriver wrapper = new DatabaseDriver(DatabaseConfig.GetProviderTypeString(), DatabaseConfig.GetConnectionString()))
+            {
+                var result = wrapper.Query("getStateAbbrevMatch", new { StateAbbreviation = "MN" }, commandType: CommandType.StoredProcedure);
+                Assert.AreEqual(1, result.Count(), "Expected 1 state abbreviation to be returned.");
+            }
+        }
+
+        /// <summary>
+        /// Successful Add Project Test
+        /// </summary>
+        [TestMethod]
+        public void AddProjectTest()
+        {
+            // Instances of the Pages Used
+            string username = Config.GetGeneralValue("Username");
+            string password = Config.GetGeneralValue("Password");
+            LoginPageModel loginPage = new LoginPageModel(this.TestObject);
+            AddProjectPageModel addprojectPage = new AddProjectPageModel(this.TestObject);
+            HomePageModel homePage = new HomePageModel(this.TestObject);
+
+            // Access Login and enter credentials
+            loginPage.OpenLoginPage();
+            loginPage.LoginWithValidCredentials(username, password);
+            HomePageModel homepage = loginPage.ByPass2FactorAuthentication();
+
+            // Assert if page is successfully loaded
+            Assert.IsTrue(homePage.IsPageLoaded());
+
+            // Successfully Add a New Project
+            addprojectPage.ReachAddProject();
+
+        }
+        }
+    }
